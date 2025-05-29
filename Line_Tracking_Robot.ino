c935dc7b062449e5e99ba0645d3444de363af092
Cynthia Wei <cynthiawei1209@gmail.com>
2025-05-29 14:36:15 -0400

    Update Line_Tracking_Robot.ino

--- Line_Tracking_Robot.ino
+++ Line_Tracking_Robot.ino
@@ -1,14 +1,14 @@
 #include <Adafruit_MotorShield.h>
-// Define Sensor
-#define IR_FRONT_LEFT A4
-#define IR_FRONT_RIGHT A0
-#define IR_FRONT_MIDDLE A2
-#define IR_BACK_RIGHT A1
-#define IR_BACK_LEFT A3
+// Define Sensor - from right to left // (A4 A3 A2 A1 A0)
+#define IR_R A0
+#define IR_RM A1
+#define IR_M A2
+#define IR_LM A3
+#define IR_L A4
 // Defining Motor Pins
-#define MOTOR_FR_TERMINAL 1
-#define MOTOR_FL_TERMINAL 2
-#define MOTOR_BR_TERMINAL 3
+#define MOTOR_FR_TERMINAL 2
+#define MOTOR_FL_TERMINAL 3
+#define MOTOR_BR_TERMINAL 1
 #define MOTOR_BL_TERMINAL 4
 // Instantiating the Motorshield
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // default board constructor
@@ -17,8 +17,12 @@ Adafruit_DCMotor *MOTOR_FL = AFMS.getMotor(MOTOR_FL_TERMINAL);
 Adafruit_DCMotor *MOTOR_FR = AFMS.getMotor(MOTOR_FR_TERMINAL);
 Adafruit_DCMotor *MOTOR_BL = AFMS.getMotor(MOTOR_BL_TERMINAL);
 Adafruit_DCMotor *MOTOR_BR = AFMS.getMotor(MOTOR_BR_TERMINAL);
-// Time variable
-int time = 0;
+
+// Global variables
+//int elapsedTime = 0;
+bool direction; // false = right, true = left
+bool stopReadingSensor = false;
+
 int timer = 0;
 char lastMove = 'o';
 bool start = false;
@@ -27,11 +31,11 @@ bool end = false;
 void setup() {
   // put your setup code here, to run once:
 
-    pinMode(IR_FRONT_RIGHT, INPUT);
-    pinMode(IR_FRONT_LEFT, INPUT);
-    pinMode(IR_FRONT_MIDDLE, INPUT);
-    pinMode(IR_BACK_LEFT, INPUT);
-    pinMode(IR_BACK_RIGHT, INPUT);
+    pinMode(IR_R, INPUT);
+    pinMode(IR_RM, INPUT);
+    pinMode(IR_M, INPUT);
+    pinMode(IR_LM, INPUT);
+    pinMode(IR_L, INPUT);
 
   // Turn on the motorshield and PWM driver
   AFMS.begin();
@@ -52,65 +56,165 @@ void setup() {
   MOTOR_BR->setSpeed(0);
   MOTOR_BR->run(RELEASE);
 
+  if(threeRightSensorsBlack()){
+    direction = true;
+  } else {
+    direction = false;
+  }
+
 }
 void loop() {
-   // 1 is black, 0 is white
-  if (digitalRead(IR_FRONT_RIGHT)==1){
-      time = 0;
-      slightTurn(255, conTurn('r'));
-  } else if (digitalRead(IR_FRONT_LEFT)==1){
-      time = 0;
-      slightTurn( conTurn('l'), 255);
-      
-  } else {
-    goForwards(190);
-    if(time > 2){
-      goForwards(255);
+// 1 is black, 0 is white5
+  
+  // if(!stopReadingSensor) {
+    if(readAllBlack()){
+        stop();
+        delay(10);
+    } else if (threeRightSensorsBlack()) {
+        // turn left
+        turn(255, 110);
+    } else if (threeLeftSensorsBlack()) {
+        turn(110,255);
+    } else if (twoRightSensorsBlack()){
+        turn(255, 65);
+    } else if(twoLeftSensorsBlack()){
+        turn(65,255);
+    } else if (oneRightSensorBlack()){
+        turn (255, 15);
+        //delay(5);
+    } else if (oneLeftSensorBlack()){
+        turn (15, 255);
+        //delay(5);
+    } else if (centered()){
+        goForwards(255);
+    } else if (readAllBlack() || readAllWhite()){
+        stop();
+        delay(10);
+        // stopReadingSensor = true;
     } 
-    conTurn('f');
-  }
-  delay(1);
-
-  // if((digitalRead(IR_FRONT_RIGHT) == 0 && digitalRead(IR_FRONT_MIDDLE) == 0 && digitalRead(IR_BACK_LEFT) == 1) ||(digitalRead(IR_FRONT_LEFT) == 1 && digitalRead(IR_FRONT_MIDDLE) == 1 && digitalRead(IR_BACK_RIGHT) == 1)){
-  //   stop();
-  //   delay(300);
+    // if(readAllBlack()){
+        // stopReadingSensor = false;
+        
   // }
-  if(digitalRead(IR_FRONT_RIGHT) == 1 && digitalRead(IR_FRONT_LEFT) == 1 && digitalRead(IR_FRONT_MIDDLE) == 1){
-    stop();
-    delay(300);
-    lastMove = 's';
+  // }
+  //digitalRead(IR_L) == 1 && digitalRead((R_R) == 1
+/*
+   if (direction){ // turn left
+      if(!stopReadingSensor){
+          if(threeRightSensorsBlack()){
+            // turn left
+            turn(150, 200);
+          } else if (twoRightSensorsBlack()){
+            // turn left but stronger turn
+            turn(100, 200);
+          } else if (oneRightSensorBlack()){
+            turn(50, 200); // turn even sharper
+          } else if (centered()) {
+            goForwards(255); // goal is that going forward never activates when turning
+          } else if (readAllBlack()){
+            stop();
+            stopReadingSensor = true;
+          }
+      }
+   } else { // turn right
+      if(!stopReadingSensor){
+          if(threeLeftSensorsBlack()){
+            // turn left
+            turn(150, 200);
+          } else if (twoLeftSensorsBlack()){
+            // turn left but stronger turn
+            turn(100, 200);
+          } else if (oneLeftSensorBlack()){
+            turn(50, 200); // turn even sharper
+          } else if (centered()) {
+            goForwards(255); // goal is that going forward never activates when turning
+          } else if (readAllBlack()){
+            stop();
+            stopReadingSensor = true;
+          }
+      }
+   }
 
-  }
 
-  if (timer % 10 == 0){
-    Serial.println(lastMove);
-    Serial.println(timer);
-
-  }
-
+*/
 }
 
-void goForwards(int speed){   // speed = 0, minimum speed, speed = 255, full speed
-  MOTOR_FL->setSpeed(speed);
-  MOTOR_FR->setSpeed(speed);
-  MOTOR_BL->setSpeed(speed);
-  MOTOR_BR->setSpeed(speed);
 
+bool threeLeftSensorsBlack(){
+  if(digitalRead(IR_L) == 1 && digitalRead(IR_LM) == 1 && digitalRead(IR_M) == 1 && (digitalRead(IR_R) == 0 || digitalRead(IR_RM) == 0)){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool threeRightSensorsBlack(){
+  if(digitalRead(IR_R) == 1 && digitalRead(IR_RM) == 1 && digitalRead(IR_M) == 1 && (digitalRead(IR_L) == 0 || digitalRead(IR_LM) == 0)){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool twoLeftSensorsBlack(){
+  if(digitalRead(IR_L) == 1 && digitalRead(IR_LM) == 1 && digitalRead(IR_M) == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool twoRightSensorsBlack(){
+  if(digitalRead(IR_R) == 1 && digitalRead(IR_RM) == 1 && digitalRead(IR_M) == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool centered(){
+  if(digitalRead(IR_R) == 0 && digitalRead(IR_RM) == 1 && digitalRead(IR_M) == 1 && digitalRead(IR_LM) == 1 && digitalRead(IR_L) == 0){
+      return true;
+  } else {
+      return false;
+  }
+}
+bool oneLeftSensorBlack(){
+  if(digitalRead(IR_L) == 1 && digitalRead(IR_LM) == 0 && digitalRead(IR_M) == 0 && digitalRead(IR_RM) == 0 && digitalRead(IR_R) == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool oneRightSensorBlack(){
+  if(digitalRead(IR_L) == 0 && digitalRead(IR_LM) == 0 && digitalRead(IR_M) == 0 && digitalRead(IR_RM) == 0 && digitalRead(IR_R) == 1){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool readAllBlack(){
+  if((digitalRead(IR_L) == 1) && (digitalRead(IR_LM) == 1) && (digitalRead(IR_M) == 1) && (digitalRead(IR_RM) == 1) && (digitalRead(IR_R) == 1)){
+    return true;
+  } else {
+    return false;
+  }
+}
+bool readAllWhite(){
+  if(digitalRead(IR_L) == 0 && digitalRead(IR_LM) == 0 && digitalRead(IR_M) == 0 && digitalRead(IR_RM) == 0 && digitalRead(IR_R) == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+void goForwards(int speed){   // speed = 0, minimum speed, speed = 255, full speed
   MOTOR_FL->run(FORWARD);
   MOTOR_FR->run(FORWARD);
   MOTOR_BL->run(FORWARD);
   MOTOR_BR->run(FORWARD);
-}
-void goBackwards(int speed){
+
   MOTOR_FL->setSpeed(speed);
   MOTOR_FR->setSpeed(speed);
   MOTOR_BL->setSpeed(speed);
   MOTOR_BR->setSpeed(speed);
 
-  MOTOR_FL->run(BACKWARD);
-  MOTOR_FR->run(BACKWARD);
-  MOTOR_BL->run(BACKWARD);
-  MOTOR_BR->run(BACKWARD);  
+  
 }
 void stop(){
   MOTOR_FL->setSpeed(0);
@@ -123,7 +227,10 @@ void stop(){
   MOTOR_BL->run(RELEASE);
   MOTOR_BR->run(RELEASE);
 }
-void slightTurn(int left, int right){   // turn right (right < left), turn left (left < right)
+void resetStop(){
+  goForwards(0);
+}
+void turn(int left, int right){  
   MOTOR_FL->setSpeed(left);
   MOTOR_FL->run(FORWARD);
 
@@ -139,61 +246,18 @@ void slightTurn(int left, int right){   // turn right (right < left), turn left 
   MOTOR_BR->setSpeed(right);
   MOTOR_BR->run(FORWARD);
 }
-void sharpTurnLeft(int left, int right){
-  MOTOR_FL->setSpeed(left);
-  MOTOR_FL->run(BACKWARD);
-
-  // Motor B
-  MOTOR_FR->setSpeed(right);
-  MOTOR_FR->run(FORWARD);
-
-  // Motor C
-  MOTOR_BL->setSpeed(left);
-  MOTOR_BL->run(BACKWARD);
-
-  // Motor D
-  MOTOR_BR->setSpeed(right);
-  MOTOR_BR->run(FORWARD);
-}
-void sharpTurnRight(int left, int right){
-  MOTOR_FL->setSpeed(left);
-  MOTOR_FL->run(FORWARD);
-
-  // Motor B
-  MOTOR_FR->setSpeed(right);
-  MOTOR_FR->run(BACKWARD);
-
-  // Motor C
-  MOTOR_BL->setSpeed(left);
-  MOTOR_BL->run(FORWARD);
-
-  // Motor D
-  MOTOR_BR->setSpeed(right);
-  MOTOR_BR->run(BACKWARD);
-}
-bool ifStop(){
-  if (digitalRead(IR_FRONT_RIGHT) == 1 && digitalRead(IR_FRONT_MIDDLE) == 1 && digitalRead(IR_BACK_LEFT) == 1){
-    stop();
-    return true;
-  } else if (digitalRead(IR_FRONT_LEFT) == 1 && digitalRead(IR_FRONT_MIDDLE) == 1 && digitalRead(IR_BACK_RIGHT) == 1){
-    stop();
-    return true;
-  } 
-  return false;
-}
-int conTurn( char currentMove){
-  int temp = 255 -200 - timer;
-  if (lastMove == currentMove){
-        timer++;
-      }
-      else {
-        timer =0;
-      }
-    lastMove = currentMove;
-  if (temp <= 0){
-    return 0;
-  }
-  
-  return temp;
-}
+// int cont( char currentMove){
+//   int temp = 255 -timer;
+//   if (lastMove == currentMove){
+//         timer++;
+//       }
+//       else {
+//         timer =0;
+//       }
+//     lastMove = currentMove;
+//   if (temp <= 0){
+//     return 0;
+//   }
+//   return 0;
+// }
 
